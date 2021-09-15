82d3ca834bf65a940c9554200eb8619b30e481c6
Wyatt726 <wyatt726@aol.com>
2021-09-15 15:56:19 -0400

    Initial backend setup sans model definitions

--- youtube_clone/youtube_clone/local_settings.py
+++ youtube_clone/youtube_clone/local_settings.py
@@ -1,1 +1,16 @@
 SECRET_KEY = 'django-insecure-h41h2_m1p2hty1(w7)=4zdoh6q@baw$#+w3de=co-%cspd=kvo'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'youtube_clone',
+        'USER': 'root',
+        'PASSWORD': 'Number11',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS':{
+            'autocommit:True'
+        }
+
+    }
+}
