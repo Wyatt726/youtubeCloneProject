2ae555715758778fa0b679c1245293f085acbc06
Kory Attleson <kory.attleson@gmail.com>
2021-09-15 16:30:28 -0500

    Created model for Youtube Clone project. Migrated to MySql database table

--- youtube_clone/youtube/models.py
+++ youtube_clone/youtube/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    video_Id: models.TextField(max_length=50)
+    comment = models.TextField(max_length=50)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    replies = models.TextField(max_length=50)
