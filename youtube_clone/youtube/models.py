3005fa1abd83ebd0abc93af55da094f7709497b9
Wyatt726 <wyatt726@aol.com>
2021-09-16 15:20:08 -0400

    commiting for merge

--- youtube_clone/youtube/models.py
+++ youtube_clone/youtube/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
-    video_Id: models.TextField(max_length=50)
+    video_Id = models.TextField(max_length=50)
     comment = models.TextField(max_length=50)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
