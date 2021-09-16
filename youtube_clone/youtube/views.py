32c337051a2043ac135b83c8bb4f37dc36a70246
Kory Attleson <kory.attleson@gmail.com>
2021-09-16 11:46:20 -0500

    Created serializers.py file, and urls.py file and able to successfully run get/post requests through Postman. Exported Postman collection to Project.

--- youtube_clone/youtube/views.py
+++ youtube_clone/youtube/views.py
@@ -1,3 +1,24 @@
+import re
+from django.db.models import manager
 from django.shortcuts import render
+import rest_framework
+from .models import Video
+from .serializers import VideoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
 
 # Create your views here.
+class VideoList(APIView):
+
+    def get(self, request):
+        video = Video.objects.all()
+        serializer = VideoSerializer(video, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = VideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
