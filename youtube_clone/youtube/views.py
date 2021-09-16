38b248e4b5591d753c436ea989f3ebf4d2bc8cd9
Kory Attleson <kory.attleson@gmail.com>
2021-09-16 12:53:22 -0500

    Added VideoDetail API to make get, put, and delete requests from the front end

--- youtube_clone/youtube/views.py
+++ youtube_clone/youtube/views.py
@@ -1,5 +1,7 @@
 import re
+from warnings import showwarning
 from django.db.models import manager
+from django.http.response import Http404
 from django.shortcuts import render
 import rest_framework
 from .models import Video
@@ -22,3 +24,29 @@ class VideoList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VideoDetail(APIView):
+
+    def  get_object(self, pk):
+        try:
+            return Video.objects.get(pk=pk)
+        except Video.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = VideoSerializer(comment)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = VideoSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        comment = self.get_object(pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
