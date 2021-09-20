b5919a953ca2c26dab86a95b7ece4c07eccebbd3
Kory Attleson <kory.attleson@gmail.com>
2021-09-20 15:32:18 -0500

    Ran git rm --cached for local_settings.py file so that Wyatt and my NAME and PASSWORD don't overwrite everytime we Push

--- youtube_clone/youtube_clone/local_settings.py
+++ youtube_clone/youtube_clone/local_settings.py
@@ -3,9 +3,9 @@ SECRET_KEY = 'django-insecure-^u!q@so5p1y2fb4(2agz)cr0lsd98%6!#+(b5#mc!8ff^_f@6d
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME': 'youtube_clone1',
+        'NAME': 'youtube_clone_database',
         'USER': 'root',
-        'PASSWORD': 'Number11',
+        'PASSWORD': 'Password123!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
